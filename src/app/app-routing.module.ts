a1024e865f861b0e7dde312bbdb4f10dce0e3b37
Ana Lu <luczynaki@gmail.com>
2017-07-07 17:56:35 -0500

    generate new song list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AddSongComponent } from './add-song/add-song.component';
+import { SongListComponent } from './song-list/song-list.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SongListComponent
+  },
   {
     path: 'new',
     component: AddSongComponent,
