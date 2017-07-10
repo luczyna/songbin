828cf0594bf83b6ec7163efe50cd22ac5c74d362
Ana Lu <luczynaki@gmail.com>
2017-07-10 12:48:19 -0500

    add a clear songs button to the navigation for debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { StorageService } from './storage/storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ StorageService ]
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private storage: StorageService) {}
+
+  public clearSavedSongs(): void {
+    this.storage.clearSongs();
+    window.alert('all saved songs have been cleared from storage');
+  }
 }
