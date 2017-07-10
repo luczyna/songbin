130cc45fbb67be0314e297668dceb89d5955e1fa
Ana Lu <luczynaki@gmail.com>
2017-07-10 12:26:19 -0500

    add song detail to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AddSongComponent } from './add-song/add-song.component';
 import { SongListComponent } from './song-list/song-list.component';
+import { SongDetailComponent } from './song-detail/song-detail.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'new',
     component: AddSongComponent,
     children: []
+  },
+  {
+    path: 'song/:id',
+    component: SongDetailComponent
   }
 ];
 
