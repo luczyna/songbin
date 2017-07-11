2f834b97cb10623cfc971115c42ef93bea3d3555
Ana Lu <luczynaki@gmail.com>
2017-07-11 17:31:44 -0500

    add tests for constant service

--- src/app/constant/constant.service.ts
+++ src/app/constant/constant.service.ts
@@ -5,7 +5,7 @@ import { Validators, ValidatorFn } from '@angular/forms';
 export class ConstantService {
   // For URL validation for Songs
   // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
-  private urlRegex: RegExp = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+  public urlRegex: RegExp = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
   private urlValidator = Validators.pattern(this.urlRegex);
 
   constructor() { }
@@ -13,6 +13,4 @@ export class ConstantService {
   public getUrlValidator(): ValidatorFn {
     return Validators.compose([Validators.required, this.urlValidator]);
   }
-
-
 }
